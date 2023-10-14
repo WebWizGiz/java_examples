1ffb35c14259260cee743e4d9d8912eb36962323
Gizem <zuehre.turhan@dci-student.org>
2023-10-14 23:41:30 +0200

    general folder structure

---
+++ java_examples_1/youtubeED/variables/variables.java
@@ -0,0 +1,10 @@
+public class variables {
+    public static void main(String[] args){
+        int sayi = 10;
+        System.out.println("ogrenci sayim: 10");
+        System.out.println("ogrenci sayim: 10");
+        System.out.println("ogrenci sayim: 10");
+        System.out.println("ogrenci sayim: 10");
+
+    }
+}
