2e3e6bef10f2670a34bc2f32c669d4ceb1f07cab
Gizem <zuehre.turhan@dci-student.org>
2023-10-15 09:16:46 +0200

    complete switchDemo

--- java_examples_1/youtubeED/switchDemo/switchDemo.java
+++ java_examples_1/youtubeED/switchDemo/switchDemo.java
@@ -1,5 +1,26 @@
 public class switchDemo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        char grade = 'B';
+
+        switch (grade) {
+            case 'A':
+                System.out.println("Mukemmel : Gectiniz.");
+                break;
+            case 'B':
+            case 'C':
+                System.out.println("Iyi : Gectiniz.");
+                break;
+            case 'D':
+                System.out.println("Fena Degil : Gectiniz.");
+                break;
+            case 'E':
+                System.out.println("Iyi Degil : Kaldiniz.");
+            case 'F':
+                System.out.println("Maalesef Kaldiniz.");
+                break;
+            default:
+                System.out.println("Gecersiz not girdiniz.");
+        }
 
 
     }
