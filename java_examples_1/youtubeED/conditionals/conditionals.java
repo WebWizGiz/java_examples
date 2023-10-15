e70358763242aa81a94b8d5c538d3a73d1b114db
Gizem <zuehre.turhan@dci-student.org>
2023-10-15 08:47:47 +0200

    create conditionals

---
+++ java_examples_1/youtubeED/conditionals/conditionals.java
@@ -0,0 +1,17 @@
+public class conditionals {
+    public static void main(String[] args){
+        int sayi = 11;
+        if(sayi<20){
+            System.out.println("sayi 20den kucuktur");
+        }else if(sayi==20){
+            System.out.println("sayi 20ye esittir");
+        }else{
+            System.out.println("sayi20den buyuktur");
+        }
+
+
+
+
+
+    }
+}
