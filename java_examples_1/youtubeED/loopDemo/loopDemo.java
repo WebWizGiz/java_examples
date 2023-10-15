06eeb05425987f8305843dcef259ead2b1a036fe
Gizem <zuehre.turhan@dci-student.org>
2023-10-15 09:30:23 +0200

    complete while example

--- java_examples_1/youtubeED/loopDemo/loopDemo.java
+++ java_examples_1/youtubeED/loopDemo/loopDemo.java
@@ -4,6 +4,15 @@ public class loopDemo {
         for (int i=1;i<10;i+=2){
             System.out.println(i);
         }
-        System.out.println("dongu bitti.");
+        System.out.println("For dongusu bitti.");
+
+        //While
+        int i = 1;
+        while (i<10){
+            System.out.println(i);
+            i+=2;
+        }
+        System.out.println("While dongusu bitti.");
+
     }
 }
