95a1a36ce8901a291f459588d512b6a8411c1c6a
Gizem <zuehre.turhan@dci-student.org>
2023-10-15 08:29:10 +0200

    complete the datatypes directory

---
+++ java_examples_1/youtubeED/datatypes/dataTypes.java
@@ -0,0 +1,14 @@
+public class dataTypes {
+    public static void main(String[] args){
+        double sayi = 12.54;
+        sayi = 13;
+        System.out.println(sayi);
+
+        char karakter = 'A';
+
+        boolean dogruMu = true;
+
+
+    }
+
+}
