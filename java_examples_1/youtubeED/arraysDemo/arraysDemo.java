22c443f47a0d545099b15aa8b7bc5b881f5e2eda
Gizem <zuehre.turhan@dci-student.org>
2023-10-15 10:18:34 +0200

    complete arrayDemo class

--- java_examples_1/youtubeED/arraysDemo/arraysDemo.java
+++ java_examples_1/youtubeED/arraysDemo/arraysDemo.java
@@ -10,14 +10,25 @@ public class arraysDemo {
         System.out.println(ogrenci3);
         System.out.println(ogrenci4);
 
-        String[] ogrenciler = new String[3];
+        System.out.println("-------------------");
+
+        String[] ogrenciler = new String[4];
         ogrenciler [0] = "Engin";
         ogrenciler [1] = "Derin";
         ogrenciler [2] = "Salih";
+        ogrenciler [3] = "Gizem";
+        //ogrenciler [4] = "Ali";
 
         for (int i=0;i<ogrenciler.length;i++){
-
+            System.out.println(ogrenciler[i]);
         }
+
+        System.out.println("-------------------");
+
+        for (String ogrenci: ogrenciler) {
+            System.out.println(ogrenci);
+        }
+
 
     }
 }
