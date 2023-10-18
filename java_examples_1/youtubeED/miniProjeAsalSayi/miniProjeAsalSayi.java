ed8ff33f1bdcaaa681af42582dab9c9708cd1152
Gizem <zuehre.turhan@dci-student.org>
2023-10-18 08:21:42 +0200

    complete miniProjeAsalSayi

--- java_examples_1/youtubeED/miniProjeAsalSayi/miniProjeAsalSayi.java
+++ java_examples_1/youtubeED/miniProjeAsalSayi/miniProjeAsalSayi.java
@@ -1,21 +1,18 @@
 public class miniProjeAsalSayi {
     public static void main(String[] args) {
 
-        // Verilen sayinin asal sayi olup olmadigini bulan prorami yaz.
+        // Verilen sayinin asal sayi olup olmadigini bulan programi yaz.
 
-        int number = 11;
+        int number = 25;
         int remainder = number % 2;
-        System.out.println(remainder);
 
         boolean isPrime = true;
 
-        if (number==1){
-            System.out.println("Sayi asal degildir.");
-            return;
+        if (number < 2){
+            System.out.println("gecersiz sayi.");
         }
-
-        if (number<1){
-            System.out.println("Gecersiz sayi.");
+        if (number == 1){
+            System.out.println("sayi asal degildir.");
         }
 
         for (int i = 2; i < number; i++) {
@@ -24,12 +21,11 @@ public class miniProjeAsalSayi {
             }
         }
 
-        if (isPrime) {
-            System.out.println("Sayi asaldir.");
-        } else {
-            System.out.println("Sayi asal degildir.");
+        if (isPrime){
+            System.out.println("sayi asaldir.");
+        }else{
+            System.out.println("sayi asal degildir.");
         }
-
 
     }
 }
