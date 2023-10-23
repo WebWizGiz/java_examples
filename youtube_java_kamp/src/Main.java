3e7254a089b0618ce43c71c593f99d92ebe6d3a9
Gizem <zuehre.turhan@dci-student.org>
2023-10-23 08:55:21 +0200

    metotlar java rules 1. commmit

--- youtube_java_kamp/src/Main.java
+++ youtube_java_kamp/src/Main.java
@@ -1,33 +1,54 @@
 public class Main {
     public static void main(String[] args) {
-//DONGULER
 
-       /* for (int i=1; i<=10; i+=2){
-            System.out.println(i);
-        }*/
+       for (int i = 0; i<4; i++){
+           mesajVer("mahmut");
+       }
 
-        int sayi = 10;
-        while (sayi<=100) {
-            System.out.println(sayi);
-            sayi = sayi +10;
-        }
-
-        int sayi2 = 11;
-
-        do{
-            System.out.println(sayi2);
-            sayi = sayi + 10;
-        }while (sayi2>100);
+       mesajVer("gizem");
+       mesajVer("gokhan");
 
 
-        String urun = "laptop";
+        hesapla();
+        hesapla();
 
-        for (int i=1; i<10; i++){
-            System.out.println(urun+i);
-        }
+        System.out.println(topla(6,3));
+        System.out.println(toplananIkiSayininYuzdeOnu(10,90));
+        topla(6,98);
+        topla(6,5);
 
+
+// DRY Do Not Repeat Yourself
 
     }
 
+
+    public static void mesajVer(String isim) {
+        System.out.println("Merhaba "+ isim);
+
+    }
+
+    public static void hesapla() {
+        System.out.println("Hesaplandi");
+    }
+
+    public static int topla(int sayi1, int sayi2){
+        int toplam = sayi1 + sayi2;
+        return toplam;
+
+
+
+    }
+    //toplanan iki sayinin %10unu bul
+    public static double toplananIkiSayininYuzdeOnu(int sayi1, int sayi2) {
+        int toplam = topla(sayi1, sayi2);
+        double sonuc = toplam * 10 / 100;
+        return sonuc;
+    }
+
+    public static double krediHesapla(double krediMiktari){
+    //hesaplamalar yapilir
+    return 125000;
+    }
 
 }
---
+++ youtube_java_kamp/src/donguler.java
@@ -0,0 +1,30 @@
+public class donguler {
+    public static void main(String[] args) {
+//DONGULER
+
+       /* for (int i=1; i<=10; i+=2){
+            System.out.println(i);
+        }*/
+
+        int sayi = 10;
+        while (sayi<=100) {
+            System.out.println(sayi);
+            sayi = sayi +10;
+        }
+
+        int sayi2 = 11;
+
+        do{
+            System.out.println(sayi2);
+            sayi = sayi + 10;
+        }while (sayi2>100);
+
+
+        String urun = "laptop";
+
+        for (int i=1; i<10; i++){
+            System.out.println(urun+i);
+        }
+
+    }
+}
