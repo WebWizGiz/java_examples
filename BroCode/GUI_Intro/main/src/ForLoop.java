e95cd58b646c22fd99cc47cd636d0b46b3e2c959
Gizem <zuehre.turhan@dci-student.org>
2023-11-02 08:55:12 +0100

    just save

--- BroCode/GUI_Intro/main/src/ForLoop.java
+++ BroCode/GUI_Intro/main/src/ForLoop.java
@@ -1,8 +1,10 @@
 public class ForLoop {
     public static void main(String[] args) {
 
-        for (int i = 10; i>=0; i--){
+        for (int i = 10; i>=0;){
             System.out.println(i);
+            i-=2;
         }
+        System.out.println("Happy new year..");
     }
 }
