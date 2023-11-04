50e852ccf7b83432ff372850f23001e1e299b8ff
Gizem <zuehre.turhan@dci-student.org>
2023-11-04 20:18:21 +0100

    just save

---
+++ BroCode/GUI_Intro/main/src/TwoD_ArrayLists.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class TwoD_ArrayLists {
+    public static void main(String[] args) {
+
+        ArrayList<ArrayList<String>> groceryList = new ArrayList<>();
+
+        ArrayList<String> bakeryList = new ArrayList<>();
+        bakeryList.add("pasta");
+        bakeryList.add("garlic bread");
+        bakeryList.add("donuts");
+
+        ArrayList<String> produceList = new ArrayList<>();
+        produceList.add("tomatoes");
+        produceList.add("zuccini");
+        produceList.add("peppers");
+
+        ArrayList<String> drinkList = new ArrayList<>();
+        drinkList.add("soda");
+        drinkList.add("coffee");
+
+
+        groceryList.add(bakeryList);
+        groceryList.add(produceList);
+        groceryList.add(drinkList);
+
+        System.out.println(groceryList.get(0).get(0)); // It's a list of list
+        System.out.println(groceryList.get(2).get(1));
+        System.out.println(groceryList);
+
+    }
+}
