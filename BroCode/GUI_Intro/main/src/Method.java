47b6cc456ec8134b4575f7a880338fd6aa05e813
Gizem <zuehre.turhan@dci-student.org>
2023-11-04 21:03:32 +0100

    just save

---
+++ BroCode/GUI_Intro/main/src/Method.java
@@ -0,0 +1,26 @@
+public class Method {
+    public static void main(String[] args) {
+
+/*        String name = "Gizem";
+        int age = 21;
+        hello(name, age);
+    }
+//PARAMETERS
+    static void hello(String title, int age){
+        System.out.println("Hello " + title);
+        System.out.println("You are " + age);*/
+
+//RETURN
+        int x = 3;
+        int y = 4;
+
+        //int z = add(x, y);
+        System.out.println(add(x,y));
+    }
+
+    static int add(int x, int y) {
+        return x + y;
+
+    }
+
+}
--- BroCode/GUI_Intro/main/src/for_eachLoop.java
+++ BroCode/GUI_Intro/main/src/for_eachLoop.java
@@ -1,6 +1,19 @@
+import java.util.ArrayList;
+
 public class for_eachLoop {
     public static void main(String[] args) {
 
+        //String[] animals = {"cat", "dog", "rat", "bird"};
+
+        ArrayList<String> animals = new ArrayList<>();
+        animals.add("cat");
+        animals.add("dog");
+        animals.add("rat");
+        animals.add("bird");
+
+        for (String i : animals){
+            System.out.println(i);
+        }
 
     }
 }
