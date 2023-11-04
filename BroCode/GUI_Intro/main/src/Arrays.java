4654b581053cf48ca6e92311a7668083e594520e
Gizem <zuehre.turhan@dci-student.org>
2023-11-04 17:26:38 +0100

    just save

---
+++ BroCode/GUI_Intro/main/src/Arrays.java
@@ -0,0 +1,20 @@
+public class Arrays {
+    public static void main(String[] args) {
+
+//        String[] cars = {"Camaro", "Corvette", "Tesla", "BMW"};
+//        cars[0] = "Mustang";
+
+//        System.out.println(cars[3]);
+
+        String[] cars = new String[3];
+        cars[0] = "Camaro";
+        cars[1] = "Corvette";
+        cars[2] = "Tesla";
+
+        for (int i = 0; i< cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+
+
+    }
+}
