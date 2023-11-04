1b99cbc6e8e40a428109b7c123b2a375d748f844
Gizem <zuehre.turhan@dci-student.org>
2023-11-04 17:55:58 +0100

    just save

---
+++ BroCode/GUI_Intro/main/src/StringMethods.java
@@ -0,0 +1,20 @@
+public class StringMethods {
+    public static void main(String[] args) {
+
+        String name = "Gizem";
+
+    //  boolean result = name.equalsIgnoreCase("gizem")
+    //  int result = name.length();
+    //  char result = name.charAt(0);
+    //  int result = name.indexOf("m");
+    //  boolean result = name.isEmpty();
+    //  String result = name.toUpperCase();
+    //  String result = name.toLowerCase();
+    //  String result = name.trim();
+        String result = name.replace('i', 'k');
+
+        System.out.println(result);
+
+
+    }
+}
