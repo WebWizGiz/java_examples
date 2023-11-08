d139ce7baf88d516d5143ee3d6b59486dc0b28af
Gizem <zuehre.turhan@dci-student.org>
2023-11-08 10:34:11 +0100

    loglama ornekleri

--- oop1/NewTopic/oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProductManager.java
+++ oop1/NewTopic/oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProductManager.java
@@ -1,15 +1,20 @@
 package oopWithNLayeredApp.business;
 
+import oopWithNLayeredApp.core.logging.Logger;
 import oopWithNLayeredApp.dataAccess.HibernateProductDao;
 import oopWithNLayeredApp.dataAccess.JdbcProductDao;
 import oopWithNLayeredApp.dataAccess.ProductDao;
 import oopWithNLayeredApp.entities.Product;
 
+import java.util.List;
+
 public class ProductManager {
     private ProductDao productDao;
+    private Logger[] loggers;
 
-    public ProductManager(ProductDao productDao) {
+    public ProductManager(ProductDao productDao, Logger[] loggers) {
         this.productDao = productDao;
+        this.loggers = loggers;
     }
 
     public void add(Product product) throws Exception {   //response request pattern
@@ -20,5 +25,10 @@ public class ProductManager {
 
 //        ProductDao productDao = new HibernateProductDao();      //interface onu implemente eden sinifin newini yani referansini tutabilir.dolayisiyla bu calisir
         productDao.add(product);                                //HibernateProductDao ve JdbcProductDao'yu interface"e implment ettigimiz icin ayni referansi tutuyorlar
+
+        for (Logger logger : loggers){                  //[db, mail]
+            logger.log(product.getName());
+        }
+
     }
 }
