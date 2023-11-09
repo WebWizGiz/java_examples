da483a007156698ab5a949acb67324883576dd7e
Gizem <zuehre.turhan@dci-student.org>
2023-11-09 23:41:40 +0100

    still ogrenci menusu

--- OkulProjesi_AhBulut/src/okulProject/Ogrenci.java
+++ OkulProjesi_AhBulut/src/okulProject/Ogrenci.java
@@ -11,11 +11,11 @@ public class Ogrenci {
     static Map<String, String> ogrenciMap = new HashMap<>();
 
     public static void feykOgrenciEkle() {
-        ogrenciMap.put("123456789411", "Ali, Can, 1980, Matematik");
-        ogrenciMap.put("123456789412", "Veli, Yan, 1981, Fizik");
-        ogrenciMap.put("123456789413", "Ayse, Can, 1982, Kimya");
-        ogrenciMap.put("123456789414", "Ali, San, 1983, Kimya");
-        ogrenciMap.put("123456789415", "Alp, Yan, 1984, Matematik");
+        ogrenciMap.put("123456789411", "Ali, Can, 2008, 3569,12, A");
+        ogrenciMap.put("123456789412", "Veli, Yan, 2008, 3559, 12, B");
+        ogrenciMap.put("123456789413", "Ayse, Can, 2007, 4569, 12, A");
+        ogrenciMap.put("123456789414", "Ali, San, 2009, 1569, 11, A");
+        ogrenciMap.put("123456789415", "Alp, Yan, 2009, 3769, 11, A");
 
     }
 
@@ -67,10 +67,17 @@ public class Ogrenci {
 
     private static void ogreciListeYazdir() {
         Set<Map.Entry<String, String>> myEntrySet = ogrenciMap.entrySet();
-        System.out.println(ogrenciMap);
+
+        System.out.println("========== OGRENCI LISTESI ==========\n" +
+                "TC No          Isim      SoyIsim     D.Yili     Okul No     Sinif    Sube");
 
         for (Map.Entry<String, String> each : myEntrySet) {
 
+
+            String eachKey = each.getKey();
+            String eachValue = each.getValue();
+            String eachValueArr[] = eachValue.split(", ");  //aralarinda virgul olan bir Stringi arraye cevirmek icin en iyi yontem .split --> "," ile indexleri bolecek
+            System.out.printf("%-12s  %-6s  %-9s  %-4s  %-4s %-2s %-2s \n", eachKey, eachValueArr[0], eachValueArr[1], eachValueArr[2], eachValueArr[3], eachValueArr[4], eachValueArr[5]);
         }
     }
-    }
+}
