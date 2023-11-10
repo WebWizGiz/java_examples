0db6f0c4375e008ddb29d375acffe37980fcdc1b
Gizem <zuehre.turhan@dci-student.org>
2023-11-10 08:54:21 +0100

    still ogrenci menusu

--- OkulProjesi_AhBulut/src/okulProject/Ogrenci.java
+++ OkulProjesi_AhBulut/src/okulProject/Ogrenci.java
@@ -11,7 +11,7 @@ public class Ogrenci {
     static Map<String, String> ogrenciMap = new HashMap<>();
 
     public static void feykOgrenciEkle() {
-        ogrenciMap.put("123456789411", "Ali, Can, 2008, 3569,12, A");
+        ogrenciMap.put("123456789411", "Ali, Can, 2008, 3569, 12, A");
         ogrenciMap.put("123456789412", "Veli, Yan, 2008, 3559, 12, B");
         ogrenciMap.put("123456789413", "Ayse, Can, 2007, 4569, 12, A");
         ogrenciMap.put("123456789414", "Ali, San, 2009, 1569, 11, A");
@@ -28,7 +28,8 @@ public class Ogrenci {
                     "\n" +
                     "\t     1- Ogrenci listesi yazdir\n" +
                     "\t     2- Soyisimden ogrenci bulma\n" +
-                    "\t     3- Bilgilerini girerek ogrenci ekleme\n" +
+                    "\t     3- Sinif ve Sube ile ogrenci bulma\n" +
+                    "\t     4- Bilgilerini girerek ogrenci ekleme\n" +
                     "\t     5- Kimlik no ile kayit silme\n" +
                     "\t     A- ANAMENU\n" +
                     "\t     Q- CIKIS\n");
@@ -41,12 +42,16 @@ public class Ogrenci {
                     ogreciListeYazdir();
                     break;
                 case "2":
+                    soyisimdenOgrenciBulma();
                     break;
-                case "3":
+                case "3":       // Sinif ve Sube ile ogrenci bulma
+                    sinifVeSubeIleOgrenciBulma();
                     break;
-                case "4":
+                case "4":       //Bilgilerini girerek ogrenci ekleme
+                    ogrenciEkle();
                     break;
-                case "5":
+                case "5":       //KimlikNo ile silme
+                    tcNoIleOgrenciSilme();
                     break;
                 case "a":
                 case "A":
@@ -65,11 +70,98 @@ public class Ogrenci {
 
     }
 
-    private static void ogreciListeYazdir() {
+    private static void tcNoIleOgrenciSilme() throws InterruptedException {
+        System.out.println("Silinecek ogrenci kimlik no giriniz: ");
+        String silinecekOgrenci = scan.nextLine();
+
+        String silinecekValue = ogrenciMap.get(silinecekOgrenci);
+
+        String sonucValue = ogrenciMap.remove(silinecekOgrenci);
+
+        try {
+            boolean sonuc = sonucValue.equals(silinecekValue);
+        } catch (Exception e) {
+            System.out.println("Istediginiz TC no ile ogrenci bulunamadi");
+    }
+
+    private static void ogrenciEkle() {
+
+        System.out.println("TC No: ");
+        String tcNo = scan.nextLine();
+        System.out.println("Isim: ");
+        String isim = scan.nextLine();
+        System.out.println("Soyisim: ");
+        String soyIsim = scan.nextLine();
+        System.out.println("Dogum Yili: ");
+        String dogumYili = scan.nextLine();
+        System.out.println("Okl No: ");
+        String okulNo = scan.nextLine();
+        System.out.println("Sinif: ");
+        String sinif = scan.nextLine();
+        System.out.println("Sube: ");
+        String sube = scan.nextLine();
+
+        String key = tcNo;
+        String value = isim + ", " + soyIsim + ", " + dogumYili + ", " + okulNo + ", " + sinif + ", " + sube + ", ";
+        ogrenciMap.put(key, value);
+
+    }
+
+    private static void sinifVeSubeIleOgrenciBulma() throws InterruptedException {
+        Set<Map.Entry<String, String>> myEntrySet = ogrenciMap.entrySet();
+
+        System.out.println("Istediginiz ogrenci SINIFINI yaziniz");
+        String istenilenSinif = scan.nextLine();
+        System.out.println("Istediginiz ogrenci SUBESINI yaziniz");
+        String istenilenSube = scan.nextLine();
+
+        System.out.println("========== OGRENCI LISTESI ==========\n" +
+                "TC No        Isim     SoyIsim   D.Yili    Okul No    Sinif   Sube");
+
+        for (Map.Entry<String, String> each : myEntrySet) {
+
+            String eachKey = each.getKey();
+            String eachValue = each.getValue();
+            String eachValueArr[] = eachValue.split(", ");  //aralarinda virgul olan bir Stringi arraye cevirmek icin en iyi yontem .split --> "," ile indexleri bolecek
+
+
+            if (istenilenSinif.equalsIgnoreCase(eachValueArr[4]) && istenilenSube.equalsIgnoreCase(eachValueArr[5])){
+                System.out.printf("%-12s  %-6s  %-9s  %-9s  %-9s %-7s %-7s \n", eachKey, eachValueArr[0], eachValueArr[1], eachValueArr[2], eachValueArr[3], eachValueArr[4], eachValueArr[5]);
+            }
+        }
+        Thread.sleep(5000);
+
+    }
+
+    private static void soyisimdenOgrenciBulma() throws InterruptedException {
+        Set<Map.Entry<String, String>> myEntrySet = ogrenciMap.entrySet();
+
+        System.out.println("Istediginiz ogrenci soyismini yaziniz");
+        String istenilenSoyisim = scan.nextLine();
+
+        System.out.println("========== OGRENCI LISTESI ==========\n" +
+                "TC No        Isim     SoyIsim   D.Yili    Okul No    Sinif   Sube");
+
+        for (Map.Entry<String, String> each : myEntrySet) {
+
+            String eachKey = each.getKey();
+            String eachValue = each.getValue();
+            String eachValueArr[] = eachValue.split(", ");  //aralarinda virgul olan bir Stringi arraye cevirmek icin en iyi yontem .split --> "," ile indexleri bolecek
+
+
+            if (istenilenSoyisim.equalsIgnoreCase(eachValueArr[1])){
+                System.out.printf("%-12s  %-6s  %-9s  %-9s  %-9s %-7s %-7s \n", eachKey, eachValueArr[0], eachValueArr[1], eachValueArr[2], eachValueArr[3], eachValueArr[4], eachValueArr[5]);
+            }
+        }
+        Thread.sleep(5000);
+
+    }
+
+    private static void ogreciListeYazdir() throws InterruptedException {
         Set<Map.Entry<String, String>> myEntrySet = ogrenciMap.entrySet();
 
         System.out.println("========== OGRENCI LISTESI ==========\n" +
-                "TC No          Isim      SoyIsim     D.Yili     Okul No     Sinif    Sube");
+                "TC No        Isim     SoyIsim   D.Yili    Okul No    Sinif   Sube");
 
         for (Map.Entry<String, String> each : myEntrySet) {
 
@@ -77,7 +169,9 @@ public class Ogrenci {
             String eachKey = each.getKey();
             String eachValue = each.getValue();
             String eachValueArr[] = eachValue.split(", ");  //aralarinda virgul olan bir Stringi arraye cevirmek icin en iyi yontem .split --> "," ile indexleri bolecek
-            System.out.printf("%-12s  %-6s  %-9s  %-4s  %-4s %-2s %-2s \n", eachKey, eachValueArr[0], eachValueArr[1], eachValueArr[2], eachValueArr[3], eachValueArr[4], eachValueArr[5]);
+            System.out.printf("%-12s  %-6s  %-9s  %-9s  %-9s %-7s %-7s \n", eachKey, eachValueArr[0], eachValueArr[1], eachValueArr[2], eachValueArr[3], eachValueArr[4], eachValueArr[5]);
         }
+        Thread.sleep(5000);
+
     }
 }
