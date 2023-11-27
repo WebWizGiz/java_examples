6d02bf1351c7ad4b0927c246ac2747b2f8f58e22
Gizem <zuehre.turhan@dci-student.org>
2023-11-27 10:01:33 +0100

    manav completed

--- KodlamaVaktiProjeler/manav/src/Manav.java
+++ KodlamaVaktiProjeler/manav/src/Manav.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Manav {
     public static void main(String[] args) {
 
@@ -13,23 +14,32 @@ public class Manav {
 
         System.out.print("Kac kilo cucumber?: ");
         double cucumberKg = scan.nextDouble();
-        System.out.println(cucumberKg + " kg cucumber " + cucumberKg*cucumber + " TL");
+        System.out.println(cucumberKg + " kg cucumber " + cucumberKg * cucumber + " TL");
 
         System.out.print("Kac kilo apple?: ");
         double appleKg = scan.nextDouble();
-        System.out.println(appleKg + " kg apple " + appleKg*apple + " TL");
+        System.out.println(appleKg + " kg apple " + appleKg * apple + " TL");
 
         System.out.print("Kac kilo zuccini?: ");
         double zucciniKg = scan.nextDouble();
-        System.out.println(zucciniKg + " kg zuccini " + zucciniKg*zuccini + " TL");
+        System.out.println(zucciniKg + " kg zuccini " + zucciniKg * zuccini + " TL");
 
         System.out.print("Kac kilo tometo?: ");
         double tometoKg = scan.nextDouble();
-        System.out.println(tometoKg + " kg tometo " + tometoKg*tometo + " TL");
+        System.out.println(tometoKg + " kg tometo " + tometoKg * tometo + " TL");
 
         System.out.print("Kac kilo eggplant?: ");
         double eggplantKg = scan.nextDouble();
-        System.out.println(eggplantKg + " kg eggplant " + eggplantKg*eggplant + " TL");
+        System.out.println(eggplantKg + " kg eggplant " + eggplantKg * eggplant + " TL");
+
+        double total = 0.0;
+        total += (cucumberKg * cucumber);
+        total += (appleKg * apple);
+        total += (zucciniKg * zuccini);
+        total += (tometoKg * tometo);
+        total += (eggplantKg * eggplant);
+
+        System.out.println("Toplam tutar: " + total + " TL");
 
     }
 }
