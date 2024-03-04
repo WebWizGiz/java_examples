d640dfdca11a61b401082c897d775ddd8c25bd99
Gizem <zuehre.turhan@dci-student.org>
2024-03-04 09:27:22 +0100

    save

---
+++ OrnekCalisma/src/CollectionExample.java
@@ -0,0 +1,29 @@
+public class CollectionExample {
+    public static void main(String[] args) {
+        // sifirdan baslar n-1'de biter dolayisiyla
+        int[] ages = new int[25];
+        //String[] names = new String[25];
+
+        //ages[3] = 18;
+        //ages[4] = 19;
+
+        System.out.println(ages[0]);
+        // System.out.println(ages[3]);
+        // System.out.println(ages[4]);
+
+        /*for (int i = 0; i < ages.length; i++) {
+            ages[i] = i * 2;
+        }
+
+        for (int i = 0; i < ages.length; i++) {
+            System.out.println(ages[i]);
+        }*/
+
+        /*int[] x = new int[]{5,4,5,6};
+        int[] y = new int[];*/
+
+
+    }
+
+
+}
